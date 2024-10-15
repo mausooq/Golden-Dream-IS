2d578a10f72aa5d4615bdaba16635613a49d14f4
mausooq <mauchoomausooq051@gmail.com>
2024-10-15 14:46:03 +0530

    folder

--- config.php
+++ config.php
@@ -1,23 +1,9 @@
-
-
-
 <?php
-// config.php
-$host = 'localhost';
-$db = 'GD'; // Replace with your database name
-$user = 'root';     // Replace with your database username
-$pass = '';     // Replace with your database password
 
-function connectToDatabase() {
-    global $host, $db, $user, $pass;
-    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-    try {
-        $pdo = new PDO($dsn, $user, $pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-        exit;
-    }
+$conn = new mysqli("localhost", "root","", "gd-gold");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
